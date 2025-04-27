ffac1fc6ac759c3a7dbca0a658479a64a956cd1b
Hari Dupati <dupatihari@gmail.com>
2025-04-27 16:37:04 +0100

    created standalone app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Hello from Standalone React App!</h1>
+      <p>Running inside an iFrame on the OS 🚀</p>
+    </div>
+  );
+}
+
+export default App;
